47d0998429c9b15bb9f53e8cecc3a0a489bdc8e1
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2025-09-08 01:56:28 +0545

    asddsfa

--- pages/album/[photoAlbum].js
+++ pages/album/[photoAlbum].js
@@ -112,7 +112,7 @@ function Album({ data, relatedAlbums }) {
 
 
         <div >
-          <h2 className="mt-6 text-2xl sm:text-3xl font-bold text-center bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-white py-3 rounded-lg shadow-lg mb-6">
+          <h2 className="mt-6 text-xl sm:text-2xl font-semibold text-center bg-black text-white py-3 rounded-lg shadow-lg mb-4">
             Related Photos
           </h2>
           <Posts posts={relatedAlbums} />
