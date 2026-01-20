90e8ede9b28337fff98c4599eaf7d05633a1d069
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2026-01-20 17:57:08 +0545

    dsafsad

--- components/Posts.js
+++ components/Posts.js
@@ -1,22 +1,31 @@
-import React from 'react'
-import PicsThumbnail from './PicsThumbnail'
+import React from "react";
+import PicsThumbnail from "./PicsThumbnail";
+import { BannedHrefs } from "@/JsonData/BannedUrls";
 
 const Posts = ({ posts }) => {
+
+  // 🔒 Remove banned posts
+  const filteredPosts = posts.filter(
+    (post) => !BannedHrefs.includes(post.href)
+  );
+
   return (
-    <div className="
-      grid 
-      grid-cols-1 
-      sm:grid-cols-2 
-      xl:grid-cols-3
-      2xl:grid-cols-4
-      3xl:grid-cols-6
-      gap-2 md:gap-3 lg:gap-4
-    ">
-      {posts.map((obj) => (
-        <PicsThumbnail key={obj.title} data={obj} />
+    <div
+      className="
+        grid 
+        grid-cols-1 
+        sm:grid-cols-2 
+        xl:grid-cols-3
+        2xl:grid-cols-4
+        3xl:grid-cols-6
+        gap-2 md:gap-3 lg:gap-4
+      "
+    >
+      {filteredPosts.map((obj) => (
+        <PicsThumbnail key={obj.href || obj.title} data={obj} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Posts
+export default Posts;
